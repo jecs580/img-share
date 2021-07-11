79ead015a4a35728ed12f98d522a05f25ab78f9b
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-07-10 20:23:34 -0400

    Subida de imagenes

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,10 +1,20 @@
+const path = require('path');
+const {randomName} = require('../helpers/libs');
+const fs = require('fs-extra');
 const ctrl  = {};
 
 ctrl.index = (req,res)=>{
     
 };
-ctrl.create = (req,res)=>{
-
+ctrl.create = async (req,res)=>{
+    const imageTempPath = req.file.path;
+    const imageName = randomName();
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const targetPath = path.resolve(`src/public/upload/${imageName}.${ext}`);
+    if(ext ==='.png'|| ext === '.jpg'|| ext==='.jpeg' || ext ==='.gif'){
+        await fs.rename(imageTempPath,targetPath);
+    }
+    res.send('works');
 };
 ctrl.like = (req,res)=>{
 
